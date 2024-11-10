9087abf96c81e1b727cb80faa56c28491a809b9c
Viky Bukovska <xbukovska@stuba.sk>
2024-11-10 22:52:42 +0100

    removing this commented methods (keepig the ones not implemented)

--- slaggFE/src/store/all/mutations.ts
+++ slaggFE/src/store/all/mutations.ts
@@ -33,19 +33,6 @@ const mutation: MutationTree<AllStateInterface> = {
     state.publicChannels = payload;
   },
 
-  // joinChannel(state, payload: object){
-  //   dbConn.addUserToChannel(state.loggedUser.user, payload);
-  //   // state.loggedUser.channels.push(payload) // TODO:: Push to pivot table
-  // },
-
-  // kickMemberFromChannel(state, payload: {member: object, channel: object}) {
-  //   dbConn.removeUserFromChannel(payload.member, payload.channel);
-  //   const channel = state.loggedUser.channels.find((ch) => ch === payload.channel);
-  //   if (channel){
-  //     channel.members = channel.members.filter((member) => member !== payload.member);
-  //   }
-  // },
-
   // sendNewMessage(state, payload: {content: string, timestamp: Date, channel: object}){
   //   const newMessage: MessageStateInterface = {
   //     content: payload.content,
@@ -69,36 +56,6 @@ const mutation: MutationTree<AllStateInterface> = {
   //   const channel = state.loggedUser.channels.find((ch) => ch === payload.channel);
   //   if (channel) {
   //     channel.messages.push(newMessage) // TODO push to messages table only
-  //   }
-  // },
-
-  // addMemberToChannel(state, payload: { member: object, channel: object }) {
-  //   dbConn.addUserToChannel(payload.member, payload.channel);
-
-  //   if (state.selectedChannel.name === payload.channel.name) {
-  //     state.selectedChannel.members = [...payload.channel.members]; // to ensure reactivity bcs it didnt reload the ui properly
-  //   }
-  // },
-
-  // addKickVoteOrKickMember(state, payload: { member: object, channel: object }) {
-  //   const kickVoteEntry = payload.channel.kickVotes.find(vote => vote.member.nickName === payload.member.nickName);
-
-  //   if (kickVoteEntry) {
-  //     kickVoteEntry.votes.push({ voter: state.loggedUser.user }); // TODO:: Add to kickVotes table and push to it
-  //   }
-  //   else {
-  //     payload.channel.kickVotes.push({
-  //       member: payload.member,
-  //       votes: [{ voter: state.loggedUser.user }]
-  //     });   // TODO:: Add to kickVotes table and push to it
-  //   }
-
-  //   if (kickVoteEntry && kickVoteEntry.votes.length >= 3) {
-  //     const index = payload.channel.kickVotes.findIndex(vote => vote.member.nickName === payload.member.nickName);
-  //     if (index > -1) {
-  //       payload.channel.kickVotes.splice(index, 1);
-  //     }
-  //     this.kickMemberFromChannel(state, payload);
   //   }
   // },
 
